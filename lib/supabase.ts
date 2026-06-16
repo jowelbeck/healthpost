722351065e60ad12c199828ca27fa84135b826b3
Joseph Okai WELBECK <jowelbeck@Josephs-MacBook-Pro.local>
2026-06-16 23:02:40 +0000

    Hardcode Supabase credentials temporarily

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://ifryrtxioeyipjjroqsz.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmcnlydHhpb2V5aXBqanJvcXN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MDI5MDQsImV4cCI6MjA5NzE3ODkwNH0.Z3V93CC6x51QjDifGdLXzG-WJEl1UCh9Ybw02b-X8dY";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
